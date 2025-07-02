cf35b80840bdd2d5f6dccd73ee4c4b57c2e3131d
MikhaNaf <mikhanaftali13@gmail.com>
2025-07-02 09:46:03 +0700

    web ent crud app

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Layout from "./components/Layout"
+import UserRegister from "./pages/UserRegister"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import DashboardLayout from "./components/DashboardLayout"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes >
+        <Route element={<Layout />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<UserRegister />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
+        <Route element={<DashboardLayout />}>
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>     
+  )
+}
+
+export default App
---
+++ src/components/DashboardLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DashboardLayout() {
+  return (
+  <div className="absolute w-full bg-blue-500 dark:hidden min-h-75" />
+  )
+}
+
+export default DashboardLayout
